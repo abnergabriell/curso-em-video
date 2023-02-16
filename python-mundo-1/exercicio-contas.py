799d46ecd0b4dcfd086cec841c0d2a9a40c5e4ab
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-16 14:37:51 -0300

    exercicio de contas

---
+++ python-mundo-1/exercicio-contas.py
@@ -0,0 +1,12 @@
+a = 3
+b = 2
+soma = a + b
+divisao =  a / b 
+multiplicacao = a * b
+subtracao = a - b
+potenciacao = a ** b
+print(f"soma entre {a} e {b} e igual a: {soma}")
+print(f"divisao entre {a} e {b} e igual a:{divisao}")
+print(f" multiplicacao entre {a} e {b} e igual a: {multiplicacao}") 
+print (f"subtracao entre {a} e {b} e igual a: {subtracao}")
+print (f"pontenciacao entre {a} e {b} e igual a: {potenciacao}")
