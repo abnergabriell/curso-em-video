52e4ba9058538b3214dd9374926a7f38527f7541
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-17 16:33:49 -0300

    desafio-1

--- python-mundo-1/desafio-1.py
+++ python-mundo-1/desafio-1.py
@@ -1,2 +1,3 @@
+# Desafio 001- Crie um script python que leia nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado. 
 nome = input ('Qual é seu nome?' )
 print("Olá " + nome + "!. Prazer em te conhecer!")
