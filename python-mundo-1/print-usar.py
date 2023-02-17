d05d01f9ec7a268305b0e05ca8fac3bf622ad313
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-17 16:00:29 -0300

    Update print-usar.py

--- python-mundo-1/print-usar.py
+++ python-mundo-1/print-usar.py
@@ -6,3 +6,7 @@ nome = 'guanabara'
 idade = 25
 peso = 75.8
 print(nome, idade, peso)
+nome = input ('Qual é seu nome?')
+idade = input ('Qual é sua idade?')
+peso = input ('Qual é seu peso?')
+print (nome, idade, peso)
