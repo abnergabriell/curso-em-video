a6f9fab3554426ec0f0be43510c68d44ee756bd4
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-17 16:58:07 -0300

    desafio 2 finalizado - trabalhando com strings

---
+++ python-mundo-1/desafio-2.py
@@ -0,0 +1,5 @@
+# Desafio 002- Crie um script python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+Dia = input ("Dia:")
+Mes = input  ("Mes:")   
+Ano = input ("Ano:")
+print("Você nasceu no dia  " + Dia  + " de " + Mes + " de " + Ano + ". Correto ?")
