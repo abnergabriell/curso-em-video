cce25ac4e253eeac381dd5ca21b85453c00e19d1
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-17 15:48:44 -0300

    Print-usando

--- python-mundo-1/print-usar.py
+++ python-mundo-1/print-usar.py
@@ -1,3 +1,9 @@
 print (7+4)
 print('7' + '4')
 print("ola, mundo!")
+print('ola' , 5)
+nome = 'guanabara'
+idade = 25
+peso = 75.8
+print(nome, idade, peso)
+-----------------------------------------------------------------------------------------------
