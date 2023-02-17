f364c009344a453ef0bd255e913739e2a4d5f291
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-17 17:38:22 -0300

    Exercício Python 003-  Aprendendo soma

---
+++ python-mundo-1/exercicio-3.py
@@ -0,0 +1,5 @@
+# Exercício Python 003- Crie um programa  que leia dois numeros e mostre a soma entre eles.
+n1 =int(input('Digite um valor: '))
+n2 =int(input('Digite um valor: '))
+s = n1 + n2
+print('A soma entre {} e {} igual a : {}!' .format(n1, n2, s))
