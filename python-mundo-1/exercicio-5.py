17b35e5461a1fb56ad77ced0346bc9ec4eeea232
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-21 00:42:18 -0300

    Exercício Python #005 - Antecessor e Sucessor

---
+++ python-mundo-1/exercicio-5.py
@@ -0,0 +1,6 @@
+# Exercício Python 005-Faça  um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n1 = int(input('Digite um número: '))
+n2 = n1 - 1
+n3 = n1 + 1
+print(f'Com base no seu número {n1}, seu antecessor é {n2} e seu sucessor é {n3}.')
