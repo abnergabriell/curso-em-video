47a52d35b303f57c3a171fa5e68a4d5811a4a779
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-21 00:46:16 -0300

    Exercício Python 006- Dobro , triplo e raiz quadrado de um algoritiomo

---
+++ python-mundo-1/exercicio-7.py
@@ -0,0 +1,6 @@
+# Exercício Python 006- Crie um algoritimo que leia um número e  mostre o seu dobro, triplo e raiz quadrada.
+n1 = int(input('Digite um número: '))
+n2 = n1 * 2
+n3 = n1 * 3
+n4 = n1 ** (1/2)
+print(f'O seu número é {n1}, o seu dobro é {n2}, o seu triplo é {n3} e a sua raiz quadrada é {n4}')
