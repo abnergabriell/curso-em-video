79429ce0e7bb7b13e74b4696215e8333b7ffe9b3
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2023-02-28 16:16:37 -0300

    Exercício Python #007 - Média Aritmética

---
+++ python-mundo-1/exercicio-7.py
@@ -0,0 +1,5 @@
+# Exercício Python 007- Desenvolva um programa que leia  as duas notas de um aluno,calcule e mostre a média.
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print(f'Devido a sua primeira nota ser {n1} e a sua segunda nota ser {n2}, a sua média é {m}.')
