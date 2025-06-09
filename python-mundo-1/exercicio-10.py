e25866f2e1dfde833e6f1d5c77624ebcea3dbf5d
Abner Gabriel Longo <125588817+abnergabriell@users.noreply.github.com>
2025-06-09 19:24:47 -0300

    voltando aos estudos

--- python-mundo-1/exercicio-10.py
+++ python-mundo-1/exercicio-10.py
@@ -1,4 +1,4 @@
 # Exercício Python 010- Crie um programa que leia  quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
 real =  float(input('Quanto de dinheiro você tem na carteira? '))
 dolar = real / 5.22
-print ('Com r${} você  pode comprar  US${}' .format(real,dolar))
+print ('Com r${} você  pode comprar  US${}' .format(real,dolar))0
